6e1886f146ad907eb9bfdb57c323d18029efaf4a
Simone Sorato <srtsmn@gmail.com>
2025-01-11 19:11:49 -0300

    day19: eks vanilla initial commits

---
+++ eks-vanilla/variables.tf
@@ -0,0 +1,34 @@
+variable "common_tags" {
+  type        = map(string)
+  description = "Common tags"
+}
+
+variable "project_name" {
+  type        = string
+  description = "The resource name sufix"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region"
+}
+
+variable "ssm_vpc" {
+  type    = string
+  default = "The main VPC from AWS SSM parameters"
+}
+
+variable "ssm_public_subnets" {
+  type        = list(string)
+  description = "Public subnets from AWS SSM parameters"
+}
+
+variable "ssm_private_subnets" {
+  type = list(string)
+  description = "Private subnets from AWS SSM parameters"
+}
+
+variable "ssm_pod_subnets" {
+  type = list(string)
+  description = "PODs subnets from AWS SSM parameters"
+}
