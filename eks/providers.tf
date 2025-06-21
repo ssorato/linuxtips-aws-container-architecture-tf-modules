412ff75665145a63fe344d9c7c83cfcb2303834f
Simone Sorato <srtsmn@gmail.com>
2025-06-21 17:49:19 -0300

    day36_38: initial commit

--- eks/providers.tf
+++ eks/providers.tf
@@ -18,7 +18,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = aws_eks_cluster.main.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.default.token
