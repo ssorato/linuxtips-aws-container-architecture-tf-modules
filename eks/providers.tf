2f1fd7b568bc0fd76ad77115145d547b9a4559b4
Simone Sorato <srtsmn@gmail.com>
2025-07-11 20:12:15 -0300

    day35: fix kubectl provider

--- eks/providers.tf
+++ eks/providers.tf
@@ -37,4 +37,5 @@ provider "kubectl" {
   host                   = aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.default.token
+  load_config_file       = false
 }
